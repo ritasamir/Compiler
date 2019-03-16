35c1ba6d81090f5ff3ce7adce58218d846969879
Rita <ritasamir2587@gmail.com>
2019-03-16 12:17:09 +0200

    Update main.cpp

--- LexicalAnalyzer/main.cpp
+++ LexicalAnalyzer/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "Elem.h"
+#define MAXSIZE 10
+using namespace std;
 
+
+Elem transitionTable [MAXSIZE][MAXSIZE];
+int startState;
+vector<int> acceptStates;
+
+int main() {
+    return 0;
+}
+void getNFA (string rulesFile){
+    //regular expression to NFA
+}
+
+void getDFA (Elem NFA[][MAXSIZE]){
+    //translate the NFA to DFA
+}
+void minimize (Elem transitionTable[][MAXSIZE]){
+    //minimize the DFA
+}
